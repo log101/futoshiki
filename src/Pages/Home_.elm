a6518e2fc7601ecb82c7d1c810e30c81859d3ed3
log101 <ffrknerdm@gmail.com>
2023-07-06 19:09:01 +0000

    add futoshiki board layout with elm html

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,51 @@
 module Pages.Home_ exposing (view)
 
-import Html
+import Html exposing (Html, div)
+import Html.Attributes exposing (class)
 import View exposing (View)
 
 
 view : View msg
 view =
     { title = "Futoshiki"
-    , body = [ Html.text "Hello, world!" ]
+    , body =
+        [ div [ class "container" ]
+            [ row
+                [ cell, verticalArrow, cell, verticalArrow, cell, verticalArrow, cell ]
+            , arrowRow [ horizontalArrow, horizontalArrow, horizontalArrow, horizontalArrow ]
+            , row
+                [ cell, verticalArrow, cell, verticalArrow, cell, verticalArrow, cell ]
+            , arrowRow [ horizontalArrow, horizontalArrow, horizontalArrow, horizontalArrow ]
+            , row
+                [ cell, verticalArrow, cell, verticalArrow, cell, verticalArrow, cell ]
+            , arrowRow [ horizontalArrow, horizontalArrow, horizontalArrow, horizontalArrow ]
+            , row
+                [ cell, verticalArrow, cell, verticalArrow, cell, verticalArrow, cell ]
+            ]
+        ]
     }
+
+
+row : List (Html msg) -> Html msg
+row =
+    div [ class "row" ]
+
+
+arrowRow : List (Html msg) -> Html msg
+arrowRow =
+    div [ class "arrow-row" ]
+
+
+cell : Html msg
+cell =
+    div [ class "cell" ] []
+
+
+verticalArrow : Html msg
+verticalArrow =
+    div [ class "vertical-arrow" ] []
+
+
+horizontalArrow : Html msg
+horizontalArrow =
+    div [ class "horizontal-arrow" ] []
